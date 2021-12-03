232adeb51eee2c9d3b814cb078df87898b354f87
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-12-03 07:05:49 +0000

    Day 3: Binary Diagnostic - Init

---
+++ Day3BinaryDiagnostic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3BinaryDiagnostic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
