f7d65a2986cf646efd8e9c25c96f757e52dfb4f6
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-12-15 06:57:02 +0200

    Day 5: Hydrothermal Venture - Parse Data

--- Day5HydrothermalVenture/Program.cs
+++ Day5HydrothermalVenture/Program.cs
@@ -6,7 +6,22 @@ namespace Day5HydrothermalVenture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Read Data into Array
+            string data = @"TestData.txt";
+            List<(int x, int y)[]> ventLines = File.ReadAllLines(data)
+                .Select(line => line.Replace(" -> ", ","))
+                .Select(line => line.Split(','))
+                .Select(line => new []
+                        {
+                            (int.Parse(line[0]), int.Parse(line[1])),
+                            (int.Parse(line[2]), int.Parse(line[3]))
+                        })
+                .ToList();
+
+            foreach ((int x, int y)[] vent in ventLines)
+            {
+                Console.WriteLine("{0},{1} -> {2},{3}", vent[0].x, vent[0].y, vent[1].x, vent[1].y);
+            }
         }
     }
 }
