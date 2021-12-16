5d9707ef1ba4adf9efed87bbe206ab5927458bf2
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-12-16 06:00:04 +0200

    Day 5: Hydrothermal Venture - WIP

--- Day5HydrothermalVenture/Program.cs
+++ Day5HydrothermalVenture/Program.cs
@@ -4,7 +4,7 @@ namespace Day5HydrothermalVenture
 {
     class Program
     {
-        private static int[,] _diagram = new int[9,9];
+        private static int[,] _diagram = new int[10,10];
         static void Main(string[] args)
         {
             // Read Data into Array
@@ -12,7 +12,7 @@ namespace Day5HydrothermalVenture
             List<(int x, int y)[]> ventLines = File.ReadAllLines(data)
                 .Select(line => line.Replace(" -> ", ","))
                 .Select(line => line.Split(','))
-                .Select(line => new []
+                .Select(line => new[]
                         {
                             (int.Parse(line[0]), int.Parse(line[1])),
                             (int.Parse(line[2]), int.Parse(line[3]))
@@ -21,17 +21,78 @@ namespace Day5HydrothermalVenture
 
             foreach ((int x, int y)[] vent in ventLines)
             {
-                Console.WriteLine("{0},{1} -> {2},{3}", vent[0].x, vent[0].y, vent[1].x, vent[1].y);
+
+                char axis = GetAxis(vent);
+                if (!Char.IsLetter(axis)) continue;
+
+                int[] range = GetRange(vent, axis);
+
+                int position = GetPosition(vent, axis);
+                PlotLines(axis, position , range);
             }
 
             WriteDiagram(_diagram);
         }
 
+        private static int GetPosition((int x, int y)[] vent, char axis)
+        {
+            int position = new int { };
+
+            if (axis == 'x') position = vent[0].x;
+            if (axis == 'y') position = vent[0].y;
+
+            return position;
+        }
+        private static int[] GetRange((int x, int y)[] vent, char axis)
+        {
+            int[] boundaries = new int[] { };
+
+            if (axis == 'x') boundaries = boundaries.Concat( new int[] { vent[0].y, vent[1].y }).ToArray();
+            if (axis == 'y') boundaries = boundaries.Concat( new int[] { vent[0].x, vent[1].x }).ToArray();
+
+            Array.Sort(boundaries);
+
+            int min = Math.Min(boundaries[0], boundaries[1]);
+            int max = Math.Max(boundaries[0], boundaries[1]) + 1;
+
+            List<int> rangeList = new List<int>();
+            int i = min;
+            while (i < max )
+            {
+                rangeList.Add(i);
+                i++;
+            }
+
+            int[] range = rangeList.ToArray();
+
+            return range;
+        }
+
+        private static char GetAxis((int x, int y)[] vent)
+        {
+            char axis = new char { };
+
+            if (vent[0].x == vent[1].x) axis = 'x';
+            if (vent[0].y == vent[1].y) axis = 'y';
+            
+            return axis;
+        }
+
+        private static void PlotLines(char axis, int position, int[] range)
+        {
+            foreach(int number in range)
+            {
+                if(axis == 'x') _diagram[number, position]++;
+                if(axis == 'y') _diagram[position, number]++;
+            }
+        }
+
         private static void WriteDiagram(int[,] diagram)
         {
-            for (int y = 0; y < 9 ; y++) 
+            Console.WriteLine();
+            for (int y = 0; y < 10 ; y++) 
             {
-                Console.WriteLine("{0, 2} {1, 2} {2, 2} {3, 2} {4, 2} {5, 2} {6, 2} {7, 2} {8, 2}",
+                Console.WriteLine("{0, 2} {1, 2} {2, 2} {3, 2} {4, 2} {5, 2} {6, 2} {7, 2} {8, 2} {9, 2}",
                                    diagram[y, 0] == 0 ? "." : diagram[y, 0],
                                    diagram[y, 1] == 0 ? "." : diagram[y, 1], 
                                    diagram[y, 2] == 0 ? "." : diagram[y, 2], 
@@ -40,7 +101,8 @@ namespace Day5HydrothermalVenture
                                    diagram[y, 5] == 0 ? "." : diagram[y, 5], 
                                    diagram[y, 6] == 0 ? "." : diagram[y, 6], 
                                    diagram[y, 7] == 0 ? "." : diagram[y, 7], 
-                                   diagram[y, 8] == 0 ? "." : diagram[y, 8] 
+                                   diagram[y, 8] == 0 ? "." : diagram[y, 8], 
+                                   diagram[y, 9] == 0 ? "." : diagram[y, 9] 
                                    );
             }
             Console.WriteLine();
